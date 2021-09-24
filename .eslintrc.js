a3487e0681dccc52bdb50c43b92d5160ddd05557
Maël <55429341+mlhoutel@users.noreply.github.com>
2021-09-24 18:56:33 +0200

    Alphatab api (#10)
    * use the official api
    
    * Update workflow to run only for main
    
    * Update workflow to run only for main
    
    * Loading fonts
    
    * Refactorisation
    
    * Rework structure
    
    * Page restructuration
    
    * Not found page
    
    * Interface
    
    * Remove # from url
    
    * Layouts
    
    * Fix router root
    
    * Global layout
    
    * Fix router warnings
    
    * Update README
    
    * Dependancies fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,10 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true,
   },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
-  ],
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/typescript/recommended", "@vue/prettier", "@vue/prettier/@typescript-eslint"],
   parserOptions: {
     ecmaVersion: 2020,
   },
@@ -17,4 +12,4 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
-};
+}
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
   publicPath: process.env.NODE_ENV === "production" ? "/Tablatures/" : "/",
+  devServer: {
+    disableHostCheck: true,
+    port: 8080,
+    public: "0.0.0.0:8080",
+  },
 }
