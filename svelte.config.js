c61095507eefe3cf767d59bef245f89dc33bc38b
Maël <55429341+mlhoutel@users.noreply.github.com>
2023-02-06 16:09:45 +0100

    build: :rocket: Change deployment target to vercel (#54)
    Deploy website on vercel instead of github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,19 @@
 import preprocess from "svelte-preprocess";
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const dev = process.env.NODE_ENV === 'development';
+// const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: [vitePreprocess(), preprocess({
         postcss: true
     })],
 
 	kit: {
-		adapter: adapter({ strict: false }),
+		adapter: adapter(),
 		paths: {
-			base: dev ? '' : '/tablatures'
+			// base: dev ? '' : '/tablatures'
 		}
 	}
 };
