cfb213538248c852219d5faf275d3463489fcf55
mlhoutel <mlhoutel@gmail.com>
2023-04-07 19:40:39 +0200

    feat: :art: Better style for the input file component

--- src/routes/select/upload/+page.svelte
+++ src/routes/select/upload/+page.svelte
@@ -24,11 +24,21 @@
 </script>
 
 <div class="flex justify-center min-h-[300px] sm:min-h-[750px] w-full px-5 dark:text-stone-300">
-	<input
-		bind:this={file}
-		on:change={handleSubmit}
-		type="file"
-		accept=".gp3, .gp4, .gp5, .gpx, .gp, .xml, .cap, .tex"
-		class="cursor-pointer border-dashed border-2 border-stone-400 dark:border-stone-300 rounded text-center w-full"
-	/>
+	<div
+		class="relative w-full border-dashed border-2 border-stone-400 dark:border-stone-300 rounded text-center"
+	>
+		<div class="absolute w-full h-full top-0 right-0 flex items-center justify-center">
+			<div class="inline-flex bg-primary text-white rounded-full px-3">
+				<i class="material-icons !text-2xl pr-2">download</i>
+				<p class="mt-1 font-bold ">Click to import file</p>
+			</div>
+		</div>
+		<input
+			bind:this={file}
+			on:change={handleSubmit}
+			type="file"
+			accept=".gp3, .gp4, .gp5, .gpx, .gp, .xml, .cap, .tex"
+			class="cursor-pointer opacity-0 w-full h-full z-[999]"
+		/>
+	</div>
 </div>
