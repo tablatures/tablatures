b8966a6ebfcc3eafe16486587b724a3648ba27ac
mlhoutel <mlhoutel@gmail.com>
2025-09-05 11:16:27 +0200

    update dependancies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,15 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'prettier'
+	],
 	plugins: ['svelte3', '@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	overrides: [{ files: ['*.svelte'], processor: 'svelte-eslint-parser' }],
 	settings: {
 		'svelte3/typescript': () => require('typescript')
 	},
