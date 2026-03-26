9d83bcea9b49fdf100a91916c8cade15faf20022
mlhoutel <mlhoutel@gmail.com>
2026-03-26 19:00:11 +0100

    feat: make deployment platform-agnostic using environment variables

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,34 @@
-import adapter from '@sveltejs/adapter-static';
+import adapterStatic from '@sveltejs/adapter-static';
+import adapterVercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
+const target = process.env.VITE_DEPLOY_TARGET || 'static';
+const base = process.env.VITE_BASE_PATH || '';
+
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: false
-		}),
+		adapter:
+			target === 'server'
+				? adapterVercel()
+				: adapterStatic({
+						pages: 'build',
+						assets: 'build',
+						fallback: 'index.html',
+						precompress: false
+					}),
+
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/tablatures' : ''
+			base
 		},
+
 		prerender: {
 			handleMissingId: 'warn',
-			handleHttpError: 'warn',
-			entries: []
+			handleHttpError: 'warn'
 		},
+
 		alias: {
 			$components: 'src/library/components',
 			$utils: 'src/library/utils',
